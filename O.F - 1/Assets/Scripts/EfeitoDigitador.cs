33107ad50348d8ab0149c83d3763e54b01002602
juliarav <145375203+juliarav@users.noreply.github.com>
2024-09-09 13:55:39 -0300

    Cutscene - 02

--- O.F - 1/Assets/Scripts/EfeitoDigitador.cs
+++ O.F - 1/Assets/Scripts/EfeitoDigitador.cs
@@ -12,29 +12,51 @@ public class EfeitoDigitador : MonoBehaviour
     private string mensagemOriginal;
     public bool imprimindo;
 
+    public float tempoEntreLetras = 0.08f;
+
     private void Awake () 
     {
-
+        TryGetComponent (out componentTexto);
+        TryGetComponent (out _audioSource);
+        mensagemOriginal = componentTexto.text;
+        componentTexto.text = "";
     }
 
     private void OnEnable () 
     {
-
+        ImprimirMensagem (mensagemOriginal);
     }
 
     private void OnDisable () 
     {
- 
+        componentTexto.text = mensagemOriginal;
+        StopAllCoroutines ();
     }
 
-    public ImprimirMensagem (string msg) 
+    public void ImprimirMensagem (string msg) 
     {
-
+        if (gameObject.activeInHierarchy) 
+        {
+            if (imprimindo) return;
+            imprimindo = true;
+            StartCoroutine (msg);
+            
+        }
     }
 
     IEnumerator LetraPorLetra (string msg)
     {
-        yield return null;
+       string mensagem = "";
+       foreach (var letra in msg) 
+       {
+          mensagem += letra;
+          componentTexto.text = mensagem;
+          _audioSource.Play ();
+          yield return new WaitForSeconds (tempoEntreLetras);
+       }
+
+       imprimindo = false;
+       StopAllCoroutines ();
     }
 
 
